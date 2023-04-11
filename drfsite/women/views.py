f31258c953fc917cb79714401937cdf870afd11c
Sygayev Arman <sygayev13@gmail.com>
2023-04-11 11:53:36 +0600

    DRF

---
+++ drfsite/women/views.py
@@ -0,0 +1,34 @@
+from rest_framework import generics, viewsets
+from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
+from .models import Women, Category
+from .serializers import WomenSerializer
+from django.forms import model_to_dict
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class WomenAPIList(generics.ListCreateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+    
+class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    permission_classes = (IsOwnerOrReadOnly, )
+    
+class WomenAPIDestroy(generics.RetrieveDestroyAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
+    permission_classes = (IsAdminOrReadOnly, )
+
+    
+    
+    
+
+
+        
+        
