e27ef128a68032bb6a5133d37d130db32f83c81f
Arman Sygayev <sygayev13@gmail.com>
2023-04-21 17:39:50 +0600

    new model

--- books/books/store/views.py
+++ books/books/store/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet 
+from store.models import BookModel
+from store.serializers import BookSerializer
+ 
+ 
+class BookViewSet(ModelViewSet):
+    queryset = BookModel.objects.all()
+    serializer_class = BookSerializer
 
-# Create your views here.
+
